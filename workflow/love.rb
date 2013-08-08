833d96a1499542d427b8b8d9a97e4fb49aae2b68
Filipe Barros <filipe41.16@gmail.com>
2013-08-08 23:46:35 +0100

    initial commit

---
+++ workflow/love.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'alfred'
+require 'lastfm'
+require 'appscript'
+
+Alfred.with_friendly_error do |alfred|
+  fb = alfred.feedback
+
+  it = Appscript.app('iTunes')
+  puts it.current_track.artist.get + '-' + it.current_track.name.get
+
+  lastfm = Lastfm.new(api_key, api_secret)
+
+  # add an feedback to test rescue feedback
+  fb.add_item({
+    :uid          => ""                     ,
+    :title        => "Rescue Feedback Test" ,
+    :subtitle     => "rescue feedback item" ,
+    :arg          => ""                     ,
+    :autocomplete => "failed"               ,
+    :valid        => "no"                   ,
+  })
+
+  puts fb.to_xml(ARGV)
+end
+
+
+
---
+++ workflow/setup.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'alfred'
+require 'lastfm'
+require 'yaml'
+
+Alfred.with_friendly_error do |alfred|
+  app_info = YAML.load_file("info.yml")
+
+  api_key = app_info['api_key']
+  api_secret = app_info['api_secret']
+
+  lastfm = Lastfm.new(api_key, api_secret)
+  token = lastfm.auth.get_token
+
+  `open http://www.last.fm/api/auth/?api_key=#{api_key}&token=#{token}`
+  sleep 30
+
+  begin
+    lastfm.auth.get_session(:token => token)['key']
+    File.open(File.join(alfred.storage_path, 'token.yml'), "w") { |file|
+      file.write("token:#{token}")
+    }
+  rescue Exception => e
+    raise e
+  end
+end
