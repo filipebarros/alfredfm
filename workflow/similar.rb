9f4d6fc5c65e2da5d8d484c66e212220e35234e2
Filipe Barros <filipe41.16@gmail.com>
2013-08-13 22:14:55 +0100

    similar artists also working

--- workflow/similar.rb
+++ workflow/similar.rb
@@ -4,24 +4,30 @@
 require 'alfred'
 require 'lastfm'
 require 'appscript'
+require 'yaml'
 
 Alfred.with_friendly_error do |alfred|
+  app_info = YAML.load_file("info.yml")
+
+  api_key = app_info['api_key']
+  api_secret = app_info['api_secret']
+
   fb = alfred.feedback
 
   it = Appscript.app('iTunes')
-  puts it.current_track.artist.get
 
   lastfm = Lastfm.new(api_key, api_secret)
 
-  # add an feedback to test rescue feedback
-  fb.add_item({
-    :uid          => ""                     ,
-    :title        => "Rescue Feedback Test" ,
-    :subtitle     => "rescue feedback item" ,
-    :arg          => ""                     ,
-    :autocomplete => "failed"               ,
-    :valid        => "yes"                  ,
-  })
-
-  puts fb.to_xml(ARGV)
+  similar = lastfm.artist.get_similar(:artist => it.current_track.artist.get)
+  similar.shift
+  similar.each { |artist|
+    fb.add_item({
+      :uid        => '',
+      :title      => artist['name'],
+      :subtitle   => "#{artist['match'].to_f * 100}% Match",
+      :arg        => artist['name'],
+      :valid      => 'yes'
+    })
+  }
+  puts fb.to_xml
 end
