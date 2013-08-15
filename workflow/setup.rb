2cbf48effc452be9cf52e90ad05e89d300ff4369
Filipe Barros <filipe41.16@gmail.com>
2013-08-15 11:50:11 +0100

    small typo

--- workflow/setup.rb
+++ workflow/setup.rb
@@ -22,7 +22,7 @@ Alfred.with_friendly_error do |alfred|
   sleep 15
 
   begin
-    user_info['sesssion'] = lastfm.auth.get_session(:token => token)['key']
+    user_info['session'] = lastfm.auth.get_session(:token => token)['key']
     File.write(File.join(alfred.storage_path, 'user_info.yml'), user_info.to_yaml)
     puts "Authentication Successful!"
   rescue Exception => e
