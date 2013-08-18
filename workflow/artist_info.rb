2c2e3ef9f0ef7b7a576fc1b6805d40f2a2c3aced
Filipe Barros <filipe41.16@gmail.com>
2013-08-18 19:53:02 +0100

    removed useless require

--- workflow/artist_info.rb
+++ workflow/artist_info.rb
@@ -5,7 +5,6 @@ require 'alfred'
 require 'lastfm'
 require 'appscript'
 require 'yaml'
-require 'time'
 
 def separate_comma(number)
   number.to_s.chars.to_a.reverse.each_slice(3).map(&:join).join(",").reverse
