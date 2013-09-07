1386fe10de1f5f94df466220c263b63378fc8ace
Filipe Barros <filipe41.16@gmail.com>
2013-09-07 20:14:09 +0100

    exception caught in similar artists

--- workflow/similar.rb
+++ workflow/similar.rb
@@ -14,32 +14,33 @@ Alfred.with_friendly_error do |alfred|
   begin
     similar = alfredfm.get_similar_artists ARGV
 
-    similar.each { |artist|
-      image = artist['image'][1]['content'].split('/')[-1]
-      icon_path = AlfredfmHelper.generate_feedback_icon artist['image'][1]['content'], :volatile_storage_path, image
+    unless similar.empty?
+      similar.each { |artist|
+        image = artist['image'][1]['content'].split('/')[-1]
+        icon_path = AlfredfmHelper.generate_feedback_icon artist['image'][1]['content'], :volatile_storage_path, image
 
-      rounded = sprintf('%.2f', artist['match']).to_f
+        rounded = sprintf('%.2f', artist['match']).to_f
 
+        fb.add_item({
+          :uid        => AlfredfmHelper.generate_uuid,
+          :title      => artist['name'],
+          :subtitle   => "#{rounded * 100}% Match",
+          :arg        => artist['name'],
+          :icon       => icon_path,
+          :valid      => 'yes'
+        })
+      }
+    else
       fb.add_item({
         :uid        => AlfredfmHelper.generate_uuid,
-        :title      => artist['name'],
-        :subtitle   => "#{rounded * 100}% Match",
-        :arg        => artist['name'],
-        :icon       => icon_path,
-        :valid      => 'yes'
+        :title      => "No artist named #{ARGV.join(' ')} found!",
+        :valid      => 'no'
       })
-    }
+    end
   rescue Appscript::CommandError
     fb.add_item({
       :uid        => AlfredfmHelper.generate_uuid,
       :title      => "iTunes currently not playing any song!",
-      :valid      => 'no'
-    })
-  rescue Exception => e
-    puts e.stacktrace
-    fb.add_item({
-      :uid        => AlfredfmHelper.generate_uuid,
-      :title      => "No artist named #{ARGV.join(' ')} found!",
       :valid      => 'no'
     })
   end
