8b436285fd9de673a45981eeb0675d2232730a59
Martin Kopischke <martin@kopischke.net>
2013-09-24 02:34:20 +0200

    Clean up gem bundle setup

---
+++ workflow/bundle/bundler/setup.rb
@@ -0,0 +1,16 @@
+gempath = File.expand_path('../../gems', __FILE__)
+File.exist?(gempath) or raise "Gem path #{gempath} not found."
+
+$LOAD_PATH.unshift File.join(gempath, 'i18n-0.6.5', 'lib')
+$LOAD_PATH.unshift File.join(gempath, 'minitest-4.7.5', 'lib')
+$LOAD_PATH.unshift File.join(gempath, 'multi_json-1.8.0', 'lib')
+$LOAD_PATH.unshift File.join(gempath, 'atomic-1.1.12', 'lib')
+$LOAD_PATH.unshift File.join(gempath, 'thread_safe-0.1.2', 'lib')
+$LOAD_PATH.unshift File.join(gempath, 'tzinfo-0.3.37', 'lib')
+$LOAD_PATH.unshift File.join(gempath, 'plist-3.1.0', 'lib')
+$LOAD_PATH.unshift File.join(gempath, 'alfred-workflow-1.8.0', 'lib')
+$LOAD_PATH.unshift File.join(gempath, 'multi_xml-0.5.5', 'lib')
+$LOAD_PATH.unshift File.join(gempath, 'httparty-0.11.0', 'lib')
+$LOAD_PATH.unshift File.join(gempath, 'xml-simple-1.1.2', 'lib')
+$LOAD_PATH.unshift File.join(gempath, 'lastfm-1.20.1', 'lib')
+$LOAD_PATH.unshift File.join(gempath, 'activesupport-4.0.0', 'lib')
